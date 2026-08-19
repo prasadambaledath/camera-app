905dc5a949560aabdba33904c933fbe812988882
Prasad Ambaledath <prasad.ambaledath@convergint.com>
2026-08-19 11:37:39 +0530

    Update base URL in Vite config and set BrowserRouter basename in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div className="app">
         <Header />
         <main className="app__main">
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/camera-check/' : '/',
+  base: mode === 'production' ? '/camera-app/' : '/',
   plugins: [react()],
   server: {
     open: true,
